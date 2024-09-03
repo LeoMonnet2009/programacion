995b6d0d42ce00dd9ef83dadf30529128418ed1b
LeoMonnet2009 <163876678+LeoMonnet2009@users.noreply.github.com>
2024-09-03 10:07:37 -0300

    Add files via upload

---
+++ carrera.py
@@ -0,0 +1,10 @@
+from Personaje_clase import Personaje
+
+ritmo = input("Ingrese el ritmo: ")
+tiro = input("Ingrese el tiro: ")
+pase = input("Ingrese el pase: ")
+regate = input("Ingrese el regate: ")
+defensa = input("Ingrese el defensa: ")
+fisico = input("Ingrese el fisico: ")
+
+p1 = Personaje("Futbolista")
